6ea74ef4e4149f2fd46dc3fc6e3ead100ffa476b
krishneandhu123-sys <krishneandhu123@gmail.com>
2026-04-09 23:42:38 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+function changeText() {
+    document.getElementById("dynamicText").innerHTML = "<h2>Thanks for visiting my portfolio 🚀</h2>";
+}
+
+function showMessage() {
+    let name = document.getElementById("name").value;
+    alert("Hello " + name + "!");
+}
